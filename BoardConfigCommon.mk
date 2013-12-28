5e352ba25ce1af43d9e77eb7de0ccabc270aed7e
Brian Hall <roboots21@gmail.com>
2013-12-27 20:08:07 -0500

    flo: update toolchain
    Change-Id: I2c84af7147e550a7083984355cb2713063342885

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,7 +34,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
 TARGET_KERNEL_SOURCE := kernel/asus/flo
 TARGET_KERNEL_CONFIG := slim_flo_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro/bin/arm-linux-gnueabihf-
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.7-13.04/bin/arm-linux-gnueabihf-
 
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_LEGACY_ALSA_AUDIO:= false
