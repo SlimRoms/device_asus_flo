d1814a374b1ea3f7cca7de6a298c84a187055e86
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-03-25 01:30:14 +0100

    We are not on KK anymore, use Launcher3
    Thanx to Griffin for pointing me in the right direction ;)
    
    Change-Id: I83a08a84ad4d3f4e36d309a6cc34ddea019bdd7e
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- aosp_flo.mk
+++ aosp_flo.mk
@@ -32,3 +32,6 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 $(call inherit-product, device/asus/flo/device.mk)
 $(call inherit-product-if-exists, vendor/asus/flo/device-vendor.mk)
 #$(call inherit-product-if-exists, vendor/qcom/proprietary/common/config/device-vendor.mk)
+
+PRODUCT_PACKAGES += \
+    Launcher3
